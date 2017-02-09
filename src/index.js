bb2ce6d5e1f491ce36840ec6569f171b00e00a6d
Grzegorz Kućmierz <gkucmierz@gmail.com>
2017-02-09 10:46:24 +0100

    rewrited proxy from scratch

--- src/index.js
+++ src/index.js
@@ -1,36 +1,37 @@
-const express = require('express');
-const fs = require('fs');
-const Http = require('node-rest-client').Client;
+const http = require('http');
+const hh = require('http-https');
+const Url = require('url');
 
-const index = fs.readFileSync('./public/index.html');
 const port = process.env.PORT || 5000;
 
-const app = express();
+http.createServer((request, response) => {
+  let url = decodeURIComponent(request.url.substr(1));
+  let options = Url.parse(url);
 
-app.listen(port, () => {
+  options.headers = request.headers;
+  // remove host as it is taken from url
+  delete options.headers.host;
+
+  hh.request(options, proxyRes => {
+    Object.keys(proxyRes.headers).map(key => {
+      response.setHeader(key, proxyRes.headers[key]);
+    });
+    forceCors(response);
+
+    proxyRes.on('data', chunk => {
+      response.write(chunk, 'binary');
+    });
+
+    proxyRes.on('end', () => {
+      response.end();
+    });
+  }).end();
+
+}).listen(port, () => {
   console.log('App now running on port ', port);
 });
 
-function forceCors(res) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+function forceCors(resp) {
+  resp.header('Access-Control-Allow-Origin', '*');
+  resp.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
 }
-
-app.get('/', function (req, res) {
-  res.header('Content-Type', 'text/html');
-  res.send(index);
-});
-
-// catch all
-app.use((req, res) => {
-  let url = decodeURIComponent(req.url.substr(1));
-  let http = new Http();
-
-  http.get(url, (data, response) => {
-    Object.keys(response.headers).map(key => res.header(key, response.headers[key]));
-    res.status(response.statusCode);
-    res.end(data);
-  });
-
-  forceCors(res);
-});
